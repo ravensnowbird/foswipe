f7f9f3ce28de4bf37ed7734bccf5a4ab4b03347f
Vivek Sampara <ravensnowbird@gmail.com>
2014-09-11 19:32:49 +0530

    Fixed devise views issue

--- lib/foswipe/engine.rb
+++ lib/foswipe/engine.rb
@@ -4,5 +4,8 @@ require 'dragonfly'
 module Foswipe
   class Engine < ::Rails::Engine
     isolate_namespace Foswipe
+    config.to_prepare do
+      Devise::SessionsController.layout "foswipe/application"
+    end
   end
 end
