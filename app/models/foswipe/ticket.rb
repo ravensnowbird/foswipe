ab15b70f7c603e280c45d7b8f287131355a5baa4
Your Name <you@example.com>
2014-09-12 17:53:16 +0530

    added relatio beetwen ticket and comments

--- app/models/foswipe/ticket.rb
+++ app/models/foswipe/ticket.rb
@@ -3,7 +3,8 @@ class Foswipe::Ticket < ActiveRecord::Base
   belongs_to :customer , :class_name => :User,:foreign_key => "client_id"
   belongs_to :agent , :class_name => :User,:foreign_key => "support_id"
   belongs_to :client
-  has_many :comments
+  has_many :ticket_comments
+  has_many :ticket_notes
   #belongs_to :support
   #dragonfly_accessor :attachment
  has_many :ticket_attachments, :dependent => :destroy
