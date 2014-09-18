bd0c92fad32da1297a45d6ac71671f4f34c7b95d
Madhu <madhusudhan@coppernine.com>
2014-09-18 19:19:06 +0530

    changes made in todos controller and ticket show

--- app/controllers/foswipe/todos_controller.rb
+++ app/controllers/foswipe/todos_controller.rb
@@ -2,11 +2,20 @@ require_dependency "foswipe/application_controller"
 
 module Foswipe
   class TodosController < ApplicationController
+    before_action :set_todo, only: [:create ,  :destroy]
     def create
-      @ticket = Foswipe::Ticket.find(params[:ticket_id])
       @ticket.todos.create(todo_params)
       respond_to do |format|
         format.html { redirect_to @ticket, notice: 'Todo was successfully Created.' }
+        format.json { head :no_content }
+      end
+    end
+    
+    def destroy
+      @todo = @ticket.todos.find(params[:id])
+      @todo.delete
+      respond_to do |format|
+        format.html { redirect_to @ticket, notice: 'Todo was successfully deleted.' }
         format.json { head :no_content }
       end
     end
@@ -15,6 +24,9 @@ module Foswipe
 
     def todo_params
       params.require(:todo).permit(:content)
+    end
+    def set_todo
+      @ticket = Foswipe::Ticket.find(params[:ticket_id])
     end
   end
 end
